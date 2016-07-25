7f09e952309c561be1fce96f16c9f13e5de8303b
Hayden Andreyka <haydenandreyka@gmail.com>
2016-07-25 15:44:47 -0700

    aaaaaaaaaaaaaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaa

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// does stuff and things aaaaaaaaa
+
+const discordjs = require('discord.js');
+const wallpaper = require('wallpaper');
+const util = require('util');
+
+var BotClient = new discordjs.Client();
+
