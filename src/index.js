e8a77b652a6681709c473f3f47367a1a4f894e23
Hayden Andreyka <haydenandreyka@gmail.com>
2016-07-25 15:48:57 -0700

    aaaaaaaaaaaaaaaaaa
    more aaaaaaaaaa with added aaaaaaaaaaaaaaaa and STUFF :confused:

--- src/index.js
+++ src/index.js
@@ -5,4 +5,31 @@ const wallpaper = require('wallpaper');
 const util = require('util');
 
 var BotClient = new discordjs.Client();
+var Config;
 
+try {
+	require('./cfg/config.json');
+} catch(er) {
+	console.error('ffs make a config file in the cfg folder');
+	process.exit(1);
+}
+
+function startAndStuff() {
+	BotClient.loginWithToken(Config.Token, (err, token) => {
+		if (err) {
+			console.error('failed to log in oh no (check yo connection or dns)');
+			process.exit(1);
+			return;	// return because idk i wanted to
+		}
+	});
+}
+
+BotClient.on('message', (msg) => {
+	// things
+});
+
+BotClient.on('ready', () => {
+	console.log('yes. started. woooohoooo');
+});
+
+startAndStuff();
