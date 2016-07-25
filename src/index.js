a4796734700c8e4473fd2dc144bb2a75dad62984
Hayden Andreyka <haydenandreyka@gmail.com>
2016-07-25 16:32:18 -0700

    this is not okay

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ const wallpaper = require('wallpaper');
 const util = require('util');
 const ostmpdir = require('os-tmpdir');
 const fs = require('fs');
+const request = require('request');
 
 var BotClient = new discordjs.Client();
 var Config;
 const tmpDir = ostmpdir() + '/discord-wallpaper/';
 
 try {
-	require('./cfg/config.json');
+	Config = require('./cfg/config.json');
 } catch(er) {
 	console.error('ffs make a config file in the cfg folder');
 	process.exit(1);
@@ -31,49 +32,30 @@ function startAndStuff() {
 }
 
 BotClient.on('message', (msg) => {
-	if (Config.ListenChannels.indexOf(msg.channel.id) <= -1)
+	if (Config.ListenChannels.indexOf(msg.channel.id) <= -1 && Config.ListenChannels.indexOf('all') <= -1)
 		return;
 	
-	if (msg.attachments.length <= 0)
+	if (!msg.attachments.length)
 		return;
 	
-	if (msg.author.equals(BotClient.user)
-		return;
+	/*if (msg.author.equals(BotClient.user))
+		return;*/
 	
-	const imageType = getImageType(msg.attachments[0]);
-	
-	if (!imageType)
-		return;
-	
-	fs.writeFileSync(tmpDir + util.format('%s.%s', msg.id, imageType), msg.attachments[0]);
-	
-	wallpaper.set(util.format('%s.%s', msg.id, imageType));
+	request.get(msg.attachments[0].url).on('response', (data) => {
+		if (data.headers['content-type'] != 'image/png')
+			return;
+		
+		data.pipe(fs.createWriteStream(tmpDir + util.format('%s.png', msg.id)));
+		
+		data.on('end', () => {
+			wallpaper.set(util.format('%s.png', msg.id));
+			console.log(util.format('wallpaper set to %s by (%s/%s)', msg.attachments[0].url, msg.author.name, msg.author.id));
+		});
+	});
 });
 
 BotClient.on('ready', () => {
 	console.log('yes. started. woooohoooo');
 });
-
-// it's ugly and uses magic numbers but who cares
-function getImageType(buffer) {
-    var int32View = new Int32Array(buffer);
-    switch(int32View[0]) {
-        case 1196314761: 
-        	return "png";
-            break;
-        case 944130375:
-        	return "gif";
-            break;
-        case 544099650:
-        	return "bmp";
-            break;
-        case -520103681:
-            return "jpg";
-            break;
-        default:
-			return false;
-            break;
-    }	
-}
 
 startAndStuff();
